fe70fed4c492f60a1ecd3209975e755b81eb1571
squidly <squidly@localhost>
2017-05-14 19:19:15 +1000

    Add hotkey assignment fail info to installation guide

---
+++ VirtualDesktop-master/samples/VirtualDesktop.Showcase/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsDesktop;
+
+namespace VirtualDesktopShowcase
+{
+	partial class MainWindow
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+
+			foreach (var id in VirtualDesktop.GetDesktops().Select(x => x.Id))
+			{
+				System.Diagnostics.Debug.WriteLine(id);
+			}
+		}
+
+		private void CreateNew(object sender, RoutedEventArgs e)
+		{
+			VirtualDesktop.Create().Switch();
+		}
+
+		private void CreateNewAndMove(object sender, RoutedEventArgs e)
+		{
+			var desktop = VirtualDesktop.Create();
+
+			this.MoveToDesktop(desktop);
+			desktop.Switch();
+		}
+
+		private void SwitchLeft(object sender, RoutedEventArgs e)
+		{
+			this.GetCurrentDesktop().GetLeft()?.Switch();
+		}
+
+		private void SwitchLeftAndMove(object sender, RoutedEventArgs e)
+		{
+			var left = this.GetCurrentDesktop().GetLeft();
+			if (left == null) return;
+
+			this.MoveToDesktop(left);
+			left.Switch();
+		}
+
+		private void SwitchRight(object sender, RoutedEventArgs e)
+		{
+			this.GetCurrentDesktop().GetRight()?.Switch();
+		}
+
+		private void SwitchRightAndMove(object sender, RoutedEventArgs e)
+		{
+			var right = this.GetCurrentDesktop().GetRight();
+			if (right == null) return;
+
+			this.MoveToDesktop(right);
+			right.Switch();
+		}
+
+		private void Pin(object sender, RoutedEventArgs e)
+		{
+			this.TogglePin();
+		}
+
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetForegroundWindow();
+	}
+}
---
+++ VirtualDesktop-master/source/VirtualDesktop/VirtualDesktopHelper.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Diagnostics;
+using WindowsDesktop.Interop;
+
+namespace WindowsDesktop
+{
+	public static class VirtualDesktopHelper
+	{
+		internal static void ThrowIfNotSupported()
+		{
+			if (!VirtualDesktop.IsSupported)
+			{
+				throw new NotSupportedException("Need to include the app manifest in your project so as to target Windows 10. And, run without debugging.");
+			}
+		}
+
+
+		public static bool IsCurrentVirtualDesktop(IntPtr handle)
+		{
+			ThrowIfNotSupported();
+
+			return ComObjects.VirtualDesktopManager.IsWindowOnCurrentVirtualDesktop(handle);
+		}
+
+		public static void MoveToDesktop(IntPtr hWnd, VirtualDesktop virtualDesktop)
+		{
+			ThrowIfNotSupported();
+
+			int processId;
+			NativeMethods.GetWindowThreadProcessId(hWnd, out processId);
+
+			if (Process.GetCurrentProcess().Id == processId)
+			{
+				var guid = virtualDesktop.Id;
+				ComObjects.VirtualDesktopManager.MoveWindowToDesktop(hWnd, ref guid);
+			}
+			else
+			{
+				try
+				{
+					IntPtr view;
+					ComObjects.ApplicationViewCollection.GetViewForHwnd(hWnd, out view);
+					ComObjects.VirtualDesktopManagerInternal.MoveViewToDesktop(view, virtualDesktop.ComObject);
+				}
+				catch (System.Runtime.InteropServices.COMException ex)
+				{
+                    if (ex.Match(HResult.TYPE_E_ELEMENTNOTFOUND))
+					    throw new ArgumentException("hWnd");
+                    throw;
+				}
+			}
+		}
+
+		public static bool IsPinnedWindow(IntPtr hWnd)
+		{
+			ThrowIfNotSupported();
+
+			var view = hWnd.GetApplicationView();
+
+			if (view == IntPtr.Zero)
+			{
+				throw new ArgumentException("hWnd");
+			}
+
+			return ComObjects.VirtualDesktopPinnedApps.IsViewPinned(view);
+		}
+
+		public static void PinWindow(IntPtr hWnd)
+		{
+			ThrowIfNotSupported();
+
+			var view = hWnd.GetApplicationView();
+
+			if (view == IntPtr.Zero)
+			{
+				throw new ArgumentException("hWnd");
+			}
+
+			if (!ComObjects.VirtualDesktopPinnedApps.IsViewPinned(view))
+			{
+				ComObjects.VirtualDesktopPinnedApps.PinView(view);
+			}
+		}
+
+		public static void UnpinWindow(IntPtr hWnd)
+		{
+			ThrowIfNotSupported();
+
+			var view = hWnd.GetApplicationView();
+
+			if (view == IntPtr.Zero)
+			{
+				throw new ArgumentException("hWnd");
+			}
+
+			if (ComObjects.VirtualDesktopPinnedApps.IsViewPinned(view))
+			{
+				ComObjects.VirtualDesktopPinnedApps.UnpinView(view);
+			}
+		}
+
+		public static void TogglePinWindow(IntPtr hWnd)
+		{
+			ThrowIfNotSupported();
+
+			var view = hWnd.GetApplicationView();
+
+			if (view == IntPtr.Zero)
+			{
+				throw new ArgumentException("hWnd");
+			}
+
+			if (ComObjects.VirtualDesktopPinnedApps.IsViewPinned(view))
+			{
+				ComObjects.VirtualDesktopPinnedApps.UnpinView(view);
+			}
+			else
+			{
+				ComObjects.VirtualDesktopPinnedApps.PinView(view);
+			}
+		}
+
+		private static IntPtr GetApplicationView(this IntPtr hWnd)
+		{
+			try
+			{
+				IntPtr view;
+				ComObjects.ApplicationViewCollection.GetViewForHwnd(hWnd, out view);
+				return view;
+			}
+			catch (System.Runtime.InteropServices.COMException ex)
+			{
+                if (ex.Match(HResult.TYPE_E_ELEMENTNOTFOUND))
+				    return IntPtr.Zero;
+                throw;
+			}
+		}
+	}
+}
